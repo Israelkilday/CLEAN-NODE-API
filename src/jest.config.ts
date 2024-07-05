65c67853663c7f258543485b8e9a64c3d75983fd
Israel Kilday <israel.kilday@yahoo.com.br>
2024-07-05 15:34:07 -0300

    chore: add pre push on husky

--- src/jest.config.ts
+++ src/jest.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "jest";
 
 const config: Config = {
+  collectCoverage: true,
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
