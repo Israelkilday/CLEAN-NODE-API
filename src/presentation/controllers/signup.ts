bd6285ed3cad226c146ec8e97ff4e169fa354a0b
Israel Kilday <israel.kilday@yahoo.com.br>
2024-07-14 15:07:53 -0300

    feat: ensure SingupController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export class SignUpController {
+  handle(httpRequest: any): any {
+    return {
+      statusCode: 400,
+    };
+  }
+}
