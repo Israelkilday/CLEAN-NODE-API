bef2f53c8b3fcc9768a1ada6f55896ca62a97257
Israel Kilday <israel.kilday@yahoo.com.br>
2024-07-14 21:22:50 -0300

    feat: ensure signupcontroller returns 400 error if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 export class SignUpController {
   handle(httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error("Missing param: name"),
-    };
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error("Missing param: name"),
+      };
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error("Missing param: email"),
+      };
+    }
   }
 }
