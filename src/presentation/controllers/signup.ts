a4b1188c39790b1a3958d27367dc89372bef36e6
Israel Kilday <israel.kilday@yahoo.com.br>
2024-08-16 21:14:15 -0300

    feat: refactor: move server error to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,6 @@
 import { InvalidParamError } from "../erros/invalid-param-error";
 import { MissingParamError } from "../erros/missing-param-error";
-import { ServerError } from "../erros/server-error";
-import { badRequest } from "../helpers/http-helper";
+import { badRequest, serverError } from "../helpers/http-helper";
 import { Controller } from "../protocols/controller";
 import { EmailValidator } from "../protocols/email-validator";
 import { HttpRequest, HttpResponse } from "../protocols/http";
@@ -33,18 +32,15 @@ export class SignUpController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError("email"));
       }
-    } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError(),
-      };
-    }
 
-    return {
-      statusCode: 200,
-      body: {
-        message: "Success",
-      },
-    };
+      return {
+        statusCode: 200,
+        body: {
+          message: "Success",
+        },
+      };
+    } catch (error) {
+      return serverError();
+    }
   }
 }
