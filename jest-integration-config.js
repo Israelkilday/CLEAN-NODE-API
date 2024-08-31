5b694fc5dc6d9d47fd7db275ac57e6badbcd6147
Israel Kilday <israel.kilday@yahoo.com.br>
2024-08-30 21:55:36 -0300

    feat: add files test of integration and unit

--- jest-integration-config.js
+++ jest-integration-config.js
@@ -1,3 +1,4 @@
-import config from "./jest.config";
-config.testMatch = ["**/.test.ts"];
-export default config;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const config = require("./jest.config");
+config.testMatch = ["**/*.test.ts"];
+module.exports = config;
--- jest-unit-config.js
+++ jest-unit-config.js
@@ -1,3 +1,4 @@
-import config from "./jest.config";
-config.testMatch = ["**/.spec.ts"];
-export default config;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const config = require("./jest.config");
+config.testMatch = ["**/*.spec.ts"];
+module.exports = config;
