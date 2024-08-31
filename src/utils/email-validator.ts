430e47b0b177f47cc20afc073e594dd53cb7795a
Israel Kilday <israel.kilday@yahoo.com.br>
2024-08-31 13:42:55 -0300

    feat: ensure ensure emailValidatorAdapter return true if validator returns true

--- src/utils/email-validator.ts
+++ src/utils/email-validator.ts
@@ -1,8 +1,8 @@
 import { EmailValidator } from "@/presentation/protocols/email-validator";
+import validator from "validator";
 
 export class EmailValidatorAdapter implements EmailValidator {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   isValid(email: string): boolean {
-    return false;
+    return validator.isEmail(email);
   }
 }
