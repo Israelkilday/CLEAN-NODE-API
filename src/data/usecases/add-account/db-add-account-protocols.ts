318ffed41e96efa0b3f34009d7ff0569649087a1
Israel Kilday <israel.kilday@yahoo.com.br>
2024-09-17 22:41:39 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,3 +1,4 @@
 export * from "@/data/protocols/encrypter";
 export * from "@/domain/models/account";
 export * from "@/domain/use-cases/add-account";
+export * from "../../protocols/add-account-repository";
--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -2,18 +2,27 @@ import {
   AccountModel,
   AddAccount,
   AddAccountModel,
+  AddAccountRepository,
   Encrypter,
 } from "./db-add-account-protocols";
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter;
+  private readonly addAccountRepository: AddAccountRepository;
 
-  constructor(encrypter: Encrypter) {
+  constructor(
+    encrypter: Encrypter,
+    addAccountRepository: AddAccountRepository,
+  ) {
     this.encrypter = encrypter;
+    this.addAccountRepository = addAccountRepository;
   }
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async add(account: AddAccountModel): Promise<AccountModel | null> {
-    await this.encrypter.encrypt(account.password);
+
+  async add(accountData: AddAccountModel): Promise<AccountModel | null> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password);
+    await this.addAccountRepository.add(
+      Object.assign({}, accountData, { password: hashedPassword }),
+    );
 
     return new Promise((resolve) => resolve(null));
   }
