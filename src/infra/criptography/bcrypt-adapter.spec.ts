ef0123d3488b21fd9847c4cf17648573b18c6fc5
Israel Kilday <israel.kilday@yahoo.com.br>
2024-11-30 20:42:24 -0300

    test: "ensure BcryptAdapter throws if bcrypt throws"

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -3,7 +3,7 @@ import { BcryptAdapter } from "./bcrypt-adapter";
 
 jest.mock("bcrypt", () => ({
   async hash(): Promise<string> {
-    return new Promise((resolve) => resolve("hash"));
+    return Promise.resolve("hash");
   },
 }));
 
@@ -24,5 +24,14 @@ describe("Bcrypt Adapter", () => {
     const sut = makeSut();
     const hash = await sut.encrypt("any_value");
     expect(hash).toBe("hash");
+  });
+
+  test("Should throw if bcrypt throws", async () => {
+    const sut = makeSut();
+    jest
+      .spyOn(bcrypt, "hash")
+      .mockImplementationOnce(() => Promise.reject(new Error()));
+    const promise = sut.encrypt("any_value");
+    await expect(promise).rejects.toThrow();
   });
 });
