7b424ab04ccd8fcd9f071882ecd34796fa26970b
Israel Kilday <israel.kilday@yahoo.com.br>
2025-03-21 23:11:05 -0300

    feat: ensure LoginController returns 400 if no email is provided

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,7 +1,7 @@
 import { AddAccountRepository } from "@/data/protocols/add-account-repository";
 import { AccountModel } from "@/domain/models/account";
 import { AddAccountModel } from "@/domain/use-cases/add-account";
-import { MongoHelper } from "../helpers/mongodb-helper";
+import { MongoHelper } from "../helpers/mongo-helper";
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
--- src/infra/db/mongodb/log-repository/log.ts
+++ src/infra/db/mongodb/log-repository/log.ts
@@ -1,5 +1,5 @@
 import { LogErrorRepository } from "@/data/protocols/log-error-repository";
-import { MongoHelper } from "../helpers/mongodb-helper";
+import { MongoHelper } from "../helpers/mongo-helper";
 
 export class LogMongoRepository implements LogErrorRepository {
   async logError(stack: string): Promise<void> {
