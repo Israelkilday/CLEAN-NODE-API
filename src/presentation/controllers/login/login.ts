60b27b342c2641c5e58440563cc29deff233d0e8
Israel Kilday <israel.kilday@yahoo.com.br>
2025-04-06 19:40:19 -0300

    refactor: remove code duplication

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -28,15 +28,6 @@ export class LoginController implements Controller {
       }
 
       const { email, password } = httpRequest.body;
-
-      if (!email) {
-        return badRequest(new MissingParamError("email"));
-      }
-
-      if (!password) {
-        return badRequest(new MissingParamError("password"));
-      }
-
       const isValid = this.emailValidator.isValid(email);
 
       if (!isValid) {
