93c05ec0746c4486f281685290701a64b5f093ce
Israel Kilday <israel.kilday@yahoo.com.br>
2025-04-06 19:24:31 -0300

    feat: ensure login controller authentication with correct values

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -6,12 +6,15 @@ import {
   HttpResponse,
 } from "@/presentation/protocols";
 import { EmailValidator } from "../signup/signup-protocols";
+import { Authentication } from "@/domain/use-cases/authentication";
 
 export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator;
+  private readonly authentication: Authentication;
 
-  constructor(emailValidator: EmailValidator) {
+  constructor(emailValidator: EmailValidator, authentication: Authentication) {
     this.emailValidator = emailValidator;
+    this.authentication = authentication;
   }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -36,6 +39,8 @@ export class LoginController implements Controller {
           resolve(badRequest(new InvalidParamError("email"))),
         );
       }
+
+      await this.authentication.auth(email, password);
 
       return Promise.resolve({ body: "Login successful", statusCode: 200 });
     } catch (error) {
