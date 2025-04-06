944743f80a8a3894e571c23e3acec0fbaea4c8d7
Israel Kilday <israel.kilday@yahoo.com.br>
2025-04-06 17:36:43 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from "@/presentation/errors";
-import { badRequest } from "@/presentation/helpers/http-helper";
+import { badRequest, serverError } from "@/presentation/helpers/http-helper";
 import {
   Controller,
   HttpRequest,
@@ -15,28 +15,31 @@ export class LoginController implements Controller {
   }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body;
+    try {
+      const { email, password } = httpRequest.body;
 
-    if (!email) {
-      return new Promise((resolve) =>
-        resolve(badRequest(new MissingParamError("email"))),
-      );
+      if (!email) {
+        return new Promise((resolve) =>
+          resolve(badRequest(new MissingParamError("email"))),
+        );
+      }
+
+      if (!password) {
+        return new Promise((resolve) =>
+          resolve(badRequest(new MissingParamError("password"))),
+        );
+      }
+
+      const isValid = this.emailValidator.isValid(email);
+      if (!isValid) {
+        return new Promise((resolve) =>
+          resolve(badRequest(new InvalidParamError("email"))),
+        );
+      }
+
+      return Promise.resolve({ body: "Login successful", statusCode: 200 });
+    } catch (error) {
+      return serverError(error as Error);
     }
-
-    if (!password) {
-      return new Promise((resolve) =>
-        resolve(badRequest(new MissingParamError("password"))),
-      );
-    }
-
-    const isValid = this.emailValidator.isValid(email);
-
-    if (!isValid) {
-      return new Promise((resolve) =>
-        resolve(badRequest(new InvalidParamError("email"))),
-      );
-    }
-
-    return Promise.resolve({ body: "Login successful", statusCode: 200 });
   }
 }
